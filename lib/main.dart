ba8bcad306b9559d1d5f43387452972963da82b9
Boyetuhin <tuhinroy881@gmail.comgit config --global user.name>
2021-08-10 22:16:59 +0530

    showing  user info

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:try13/ApiResponse.dart';
 import 'package:try13/second.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -15,15 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -75,13 +69,16 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               OutlinedButton.icon(
                   onPressed: () {
-                    login();
+                   login() ;
                   },
                   icon: Icon(
                     Icons.login,
                     size: 18,
                   ),
-                  label: Text("Login")),
+                  label: Text("Login"),
+                  
+                  ),
+               
             ],
           ),
         )),
@@ -101,9 +98,9 @@ class _MyHomePageState extends State<MyHomePage> {
       if (response.statusCode == 200) {
         print("Correct");
         print(response.body);
-        jsonResponse = json.decode(response.body);
+        jsonResponse = json.decode(response.body.toString());
         print(jsonResponse);
-        Navigator.push(context, MaterialPageRoute(builder: (context)=> Second()));
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>Second(response: ApiResponse.fromJson(response.data)));
       } else {
         print("Wronggooooooooooooooooooooooooooo");
         print(response.body);
@@ -115,4 +112,9 @@ class _MyHomePageState extends State<MyHomePage> {
           .showSnackBar(SnackBar(content: Text("Blank field is not allowed")));
     }
   }
+
+
+
+
+  
 }
